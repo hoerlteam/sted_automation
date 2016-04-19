82aadd4d28b790bf4e1f3f7944fa9297a1bc128e
David Hörl <hoerldavid@gmail.com>
2016-04-19 16:11:44 +0200

    added method signature for generate_grid and test main

---
+++ Util/tile_util.py
@@ -0,0 +1,10 @@
+
+def generate_grid ( area_min, area_max, fov_dimensions ):
+    '''
+    get a list of coordinates of fields of view (x,y of upper left corner) covering the area specified by area_min and area_max
+    :param area_min: list of area minimum coordinates (x0, y0)
+    :param area_max: list of area maximum coordinates (x1, y1)
+    :param fov_dimensions: list, dimensions of field of view (width, height)
+    :return:
+    '''
+    return None
---
+++ grid_test.py
@@ -0,0 +1,11 @@
+from util import tile_util
+
+def main():
+    min_coords = [0, 0]
+    max_coords = [25, 10]
+    fov_size = [5, 5]
+
+    print(tile_util.generate_grid(min_coords, max_coords, fov_size))
+
+if __name__ == '__main__':
+    main()
