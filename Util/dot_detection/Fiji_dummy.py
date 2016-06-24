34e26e57eb7a8e5894f1d5b88529f07137efacd5
LAyer_8 <pascalbawidamann@googlemail.com>
2016-06-24 13:40:50 +0200

    implemented fiji_dummy

---
+++ Util/dot_detection/Fiji_dummy.py
@@ -0,0 +1,32 @@
+import random
+import numpy as np
+
+
+def fiji_dummy(fov):
+    """
+    Random function that mimics the behaviour of the Fiji_Dot detection function in order to test the
+    functionality of the rest of the independent of the actual samples
+    :param fov: list/tuple of 2 parameters that mimics the behaviour of the real fov
+    :return:
+    """
+    xfov = fov[0]
+    yfov = fov[1]
+    fspots = []
+    a = random.randint(0, 5)
+    for i in range(a):
+        ffs1 = ((random.randint(1, 100))/100)
+        ffs2 = ((random.randint(1, 100))/100)
+        b = ([((xfov-random.randint(0, xfov)) + ffs1), ((yfov-random.randint(0, yfov))+ ffs2), 0])
+        fspots.append(b)
+    return fspots
+
+
+def save_coords_to_temp(coords, tfile="coords-temp"):
+    file = open(str(tfile), 'w')
+    file.write(str(coords))
+    file.close()
+
+
+def do_dummy_stuff(fov):
+    a = fiji_dummy(fov)
+    save_coords_to_temp(a)
