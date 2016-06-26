ddbdd45fde0807897ac994c3956361c4df88359e
LAyer_8 <pascalbawidamann@googlemail.com>
2016-06-26 21:07:24 +0200

    implemented fiji_dummy

--- Util/dot_detection/Fiji_dummy.py
+++ Util/dot_detection/Fiji_dummy.py
@@ -12,11 +12,11 @@ def fiji_dummy(fov):
     xfov = fov[0]
     yfov = fov[1]
     fspots = []
-    a = random.randint(1, 5)
+    a = random.randint(0, 5)
     for i in range(a):
         ffs1 = ((random.randint(1, 100))/100)
         ffs2 = ((random.randint(1, 100))/100)
-        b = ([((xfov-random.randint(0, xfov)) + ffs1), ((yfov-random.randint(0, yfov))+ ffs2)])
+        b = ([((xfov-random.randint(0, xfov)) + ffs1), ((yfov-random.randint(0, yfov))+ ffs2), 0])
         fspots.append(b)
     return fspots
 
@@ -27,6 +27,6 @@ def save_coords_to_temp(coords, tfile="coords-temp"):
     file.close()
 
 
-def do_dummy_stuff(fov, outfile):
+def do_dummy_stuff(fov):
     a = fiji_dummy(fov)
-    save_coords_to_temp(a, outfile)
+    save_coords_to_temp(a)
