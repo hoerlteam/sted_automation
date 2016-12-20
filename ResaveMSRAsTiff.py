c5fb4514380adfe6b064b06349a9e6476223b6fe
David Hörl <hoerldavid@gmail.com>
2016-12-20 19:09:32 +0100

    now in ImageJ2 Style

--- ResaveMSRAsTiff.py
+++ ResaveMSRAsTiff.py
@@ -1,47 +1,24 @@
-import re, os, sys, itertools
+# @String path
+# @boolean(value=false) allseries
+
+import sys, os
+
+sys.path.append(os.getcwd())
+import ij_io
+
 from ij.io import DirectoryChooser, OpenDialog
-from ij import IJ, ImagePlus
-from loci.plugins import BF
-
-# manually import ImporterOptions, as the package name contains the "in" constant
-ImporterOptions = __import__("loci.plugins.in.ImporterOptions", globals(), locals(), ['object'], -1)
-
-def importMSR(path):
-    '''
-    open MSR files
-    returns array of stacks
-    '''
-
-    try:
-        io = ImporterOptions()
-        io.setId(path)
-        io.setOpenAllSeries(True)
-        imps = BF.openImagePlus(io)
-    except ImagePlus:
-        IJ.log("ERROR while opening image file " + path)
-
-    return (imps)
 
 def main():
-	dc = DirectoryChooser("Choose directory to process!")
-	inputDir = dc.getDirectory()
+    if path == None:
+        dc = DirectoryChooser("Choose directory to process!")
+        inputDir = dc.getDirectory()
 
-	if not inputDir:
-		return
+        if not inputDir:
+            return
+    else:
+        inputDir = path
 
-	# create output directory
-	if not os.path.exists(os.path.join(inputDir, "tiffs")):
-		os.makedirs(os.path.join(inputDir, "tiffs"));
-	
-	files = [f for f in os.walk(inputDir).next()[2] if f.endswith('.msr')]
-
-	for f in files:
-		imps = importMSR(os.path.join(inputDir, f))
-
-		for i in range(len(imps)):
-			IJ.saveAsTiff(imps[i], os.path.join(inputDir, 'tiffs', f + 'ch' + str(i) + '.tif'))
-
-	print('-- ALL DONE --')
+    ij_io.resave_msr_folder_as_tiff(inputDir, allseries)
 
 if __name__ == '__main__':
-	main()
+    main()
