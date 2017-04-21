4fb5ca4183d671b69c6b5156d059fbc756f0a59e
David Hörl <hoerldavid@gmail.com>
2017-04-21 16:25:34 +0200

    added quadratic refinement of localizations

--- spot_util.py
+++ spot_util.py
@@ -1,6 +1,7 @@
 from skimage.feature import blob_dog, blob_log, blob_doh
 from scipy import ndimage, spatial, stats
 import numpy as np
+from numpy.linalg import inv
 import skimage
 
 
@@ -12,6 +13,46 @@ def mean_along_axis(img, axis):
     #print(axes)
     profile = np.mean(img, axes)
     return profile
+
+
+def refine_point(img, guess, maxiter=10):
+    ones = tuple([1 for _ in guess])
+
+    img_ = img
+    guess_ = guess
+
+    if np.any(np.equal(guess, np.array(img.shape) - 1)):
+        guess_ = guess_ + 1
+        img_ = np.pad(img, 1, 'reflect')
+
+    idxes = [tuple(range((g - 1), (g + 2))) for g in guess_]
+
+    cut = img_[np.ix_(*idxes)]
+    gr = np.gradient(cut)
+    dx = np.array([gr[i][ones] for i in range(len(guess))])
+
+    hessian = np.zeros((len(guess), len(guess)))
+    for i in range(len(guess)):
+        for j in range(len(guess)):
+            hessian[i, j] = np.gradient(gr[i], axis=j)[ones]
+
+    try:
+        hinv = inv(hessian)
+    except np.linalg.LinAlgError:
+        return guess
+
+    # FIXME: why div/2 here
+    res = -hinv.dot(dx) / 2
+    if np.any(np.abs(res) >= 0.5):
+        if maxiter > 1:
+            return refine_point(img, np.array(guess + np.sign(np.round(res)) * ones, dtype=int), maxiter - 1)
+        else:
+            if np.any(np.abs(res) >= 1):
+                return guess
+            else:
+                return guess + res
+
+    return guess + res
 
 
 def focus_in_stack(img, pixsize_z):
