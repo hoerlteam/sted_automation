c163f8f250336b37db07699e3858c1ed325e5080
RESOLFT <RESOLFT>
2018-01-08 14:21:55 +0100

    added simple yellow pair finder

--- spot_util.py
+++ spot_util.py
@@ -122,6 +122,12 @@ def single_finder_inner(stack1, pix_sig, threshold, invertAxes, normalize):
             res.append(list(p))
     return res
 
+def pair_finder_yellow(ms, pix_sig=3, thresholds=[0.01, 0.01], normalize=True, median_thresholds=[3,3], median_radius=5):
+    stack1 = ms.stack(0).data()[0,:,:,:]
+    stack2 = ms.stack(1).data()[0,:,:,:]
+    stack1 = np.array(stack1, np.float)
+    stack2 = np.array(stack2, np.float)
+    return pair_finder_yellow_inner(stack1, stack2, pix_sig, thresholds, True, normalize,median_thresholds, median_radius)
 
 def pair_finder(ms, pix_sig=3, thresholds=[0.01, 0.01], normalize=True, median_thresholds=[3,3], median_radius=5):
     # get images in both channels
@@ -132,6 +138,31 @@ def pair_finder(ms, pix_sig=3, thresholds=[0.01, 0.01], normalize=True, median_t
 
     return pair_finder_inner(stack1, stack2, pix_sig, thresholds, True, normalize,median_thresholds, median_radius)
 
+def pair_finder_yellow_inner(stack1, stack2, pix_sig, threshold, invertAxes, normalize, median_thresholds, median_radius):
+    # detect blobs via Laplacian-of-Gaussian (only blobs brighter than threshold)
+    sig = pix_sig / np.sqrt(2)
+    dets1 = detect_blobs(stack1, [sig, sig, sig], threshold[0], normalize, median_thresholds[0], median_radius)
+    dets1 = list(dets1)
+    dets2 = detect_blobs(stack2, [sig, sig, sig], threshold[1], normalize, median_thresholds[1], median_radius)
+    dets2 = list(dets2)
+    
+    dets = dets1 + dets2
+    # did not find any spots in any of the channels -> return empty results
+    if (len(dets) == 0):
+        return []
+
+    # put spots in kd-tree for fast nearesr neighbor calculations
+    kd = spatial.KDTree(dets)
+    # if spots (in any channel) are closer than 3 pixels, pick the brighter of the two
+    # quick hack: we take the brighter spot in averaged channels
+    cleanup_kdtree((stack1 + stack2) / 2, kd, dets, 3)
+    res = []
+    for p in dets:
+        if invertAxes:
+            res.append(list(p)[-1::-1])
+        else:
+            res.append(list(p))
+    return res
 
 def pair_finder_inner(stack1, stack2, pix_sig, threshold, invertAxes, normalize, median_thresholds, median_radius):
     # detect blobs via Laplacian-of-Gaussian (only blobs brighter than threshold)
