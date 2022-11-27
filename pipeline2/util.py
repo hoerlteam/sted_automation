9386e6c7b194fe60bad46bf93bd15f09775d97be
David Hörl <hoerl@bio.lmu.de>
2022-11-27 16:26:52 +0100

    small changes and testing code on microscope

--- pipeline2/util.py
+++ pipeline2/util.py
@@ -1,6 +1,7 @@
 import json
 import signal
 import collections
+import threading
 from copy import deepcopy
 
 
@@ -16,14 +17,18 @@ class DelayedKeyboardInterrupt():
         self.pipeline = pipeline
 
     def __enter__(self):
-        self.old_handler = signal.getsignal(signal.SIGINT)
-        signal.signal(signal.SIGINT, self.handler)
+        # signal handling only works on main thread, do nothing if pipeline is running in another
+        if threading.current_thread() is threading.main_thread():
+            self.old_handler = signal.getsignal(signal.SIGINT)
+            signal.signal(signal.SIGINT, self.handler)
 
     def handler(self, sig, frame):
         self.pipeline.interrupted = True
 
     def __exit__(self, type, value, traceback):
-        signal.signal(signal.SIGINT, self.old_handler)
+        if threading.current_thread() is threading.main_thread():
+            signal.signal(signal.SIGINT, self.old_handler)
+
 
 def dump_JSON(d, path):
     """
