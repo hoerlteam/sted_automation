20473a74a1c615949427f8143db8cb5e67469366
David Hoerl <hoerldavid@gmail.com>
2025-01-22 18:16:08 +0100

    add multistep spot pair imaging with updated code, needs testing

--- autosted/callback_buildingblocks/repetition.py
+++ autosted/callback_buildingblocks/repetition.py
@@ -5,21 +5,27 @@ class ResultsRepeater:
     E.g., can be used to image each location returned by a spot or ROI detector multiple times.
     """
 
-    def __init__(self, wrapped_callback, n, nested=False):
+    def __init__(self, wrapped_callback, n, make_nested=False, wrapped_callback_is_nested=False):
         self.wrapped_callback = wrapped_callback
         self.n = n
-        self.nested = nested
+        self.make_nested = make_nested
+        self.wrapped_callback_is_nested = wrapped_callback_is_nested
 
     def __call__(self):
-        value_sets = self.wrapped_callback()
+        results = self.wrapped_callback()
         repeated_values = []
 
-        for values in value_sets:
+        for values in results:
 
             # case 1: add multiple copies of values wrapped in a tuple,
             # this way, the nested values can become configurations in a wrapping building block
-            if self.nested:
-                repeated_values.append((values,) * self.n)
+            if self.make_nested:
+                # case 1a: results are already nested, just repaet 
+                if self.wrapped_callback_is_nested:
+                    repeated_values.append(values * self.n)
+                # case 1b: assume single results, we wrap in tuple and repeat
+                else:
+                    repeated_values.append((values,) * self.n)
             # case 2 (default): just add multiple copies, that is intended to result in multiple configurations
             else:
                 for _ in range(self.n):
